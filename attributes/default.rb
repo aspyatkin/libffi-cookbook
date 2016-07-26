5f549da28e666c16824938417fe11ed6b158a9a9
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-07-26 09:52:01 +0400

    Minor refactoring

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,18 +1,18 @@
-id = :libffi
+id = 'libffi'
 
 case node['platform_family']
 when 'rhel'
-  default[id][:packages] = %w{ libffi }
-  default[id][:devel_packages] = %w{ libffi-devel }
-  default[id][:package_manager] = 'yum'
+  default[id]['packages'] = %w( libffi )
+  default[id]['devel_packages'] = %w( libffi-devel )
+  default[id]['package_manager'] = 'yum'
 when 'debian'
-  default[id][:packages] = %w{ libffi6 }
-  default[id][:devel_packages] = %w{ libffi-dev }
-  default[id][:package_manager] = 'apt'
+  default[id]['packages'] = %w( libffi6 )
+  default[id]['devel_packages'] = %w( libffi-dev )
+  default[id]['package_manager'] = 'apt'
 else
-  default[id][:packages] = %w{ libffi }
-  default[id][:devel_packages] = %w{ libffi-devel }
-  default[id][:package_manager] = 'yum'
+  default[id]['packages'] = %w( libffi )
+  default[id]['devel_packages'] = %w( libffi-devel )
+  default[id]['package_manager'] = 'yum'
 end
 
-default[id][:install_devel] = true
+default[id]['install_devel'] = true
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,14 +1,14 @@
-id = :libffi
-include_recipe node[id][:package_manager]
+id = 'libffi'
+include_recipe node[id]['package_manager']
 
-node[id][:packages].each do |pkg|
+node[id]['packages'].each do |pkg|
   package pkg do
     action :install
   end
 end
 
-if node[id][:install_devel] == true
-  node[id][:devel_packages].each do |pkg|
+if node[id]['install_devel']
+  node[id]['devel_packages'].each do |pkg|
     package pkg do
       action :install
     end
